1eceba05f1689fcd03defa11d41615c2682a62ee
Wei Jiang <jiangwei909@gmail.com>
2011-02-22 22:31:40 +0800

    修改了部分效果

--- single.php
+++ single.php
@@ -19,13 +19,13 @@ get_header(); ?>
 					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
 				</div><!-- #nav-above -->
 
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h1 class="entry-title"><?php the_title(); ?></h1>
-
-					<div class="entry-meta">
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>  style="background-color:#F6F9FD;">
+					<h1 class="entry-title" style=" text-align: center;" ><?php the_title(); ?></h1>
+					<hr>
+					<div class="entry-meta" style=" text-align: center;">
 						<?php twentyten_posted_on(); ?>
 					</div><!-- .entry-meta -->
-
+					
 					<div class="entry-content">
 						<?php the_content(); ?>
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>

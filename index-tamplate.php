91d218d1ae562c9bb99ef566bea21e7f0c602436
Wei Jiang <jiangwei909@gmail.com>
2011-05-18 00:06:25 +0800

    增加了将文章(2篇)放在首页的顶部位置,需要设置分类为置顶文章,然后发布文章的时候选其为分类之一

--- index-tamplate.php
+++ index-tamplate.php
@@ -19,6 +19,70 @@ get_header(); ?>
 
 					<div class="entry-content">
 						   <?php the_content(); ?>
+<div id="toparticle">
+<?php
+
+//截断函数  
+function chinasuber($sourcestr,$cutlength){ 
+   $returnstr='';
+   $i=0;
+   $n=0;
+   $str_length=strlen($sourcestr);//字符串的字节数
+   while (($n<$cutlength) and ($i<=$str_length))
+   {
+      $temp_str=substr($sourcestr,$i,1);
+      $ascnum=Ord($temp_str);//得到字符串中第$i位字符的ascii码
+      if ($ascnum>=224)    //如果ASCII位高与224，
+      {
+         $returnstr=$returnstr.substr($sourcestr,$i,3); //根据UTF-8编码规范，将3个连续的字符计为单个字符        
+         $i=$i+3;            //实际Byte计为3
+         $n++;            //字串长度计1
+      }
+      elseif ($ascnum>=192) //如果ASCII位高与192，
+      {
+         $returnstr=$returnstr.substr($sourcestr,$i,2); //根据UTF-8编码规范，将2个连续的字符计为单个字符
+         $i=$i+2;            //实际Byte计为2
+         $n++;            //字串长度计1
+      }
+      elseif ($ascnum>=65 && $ascnum<=90) //如果是大写字母，
+      {
+         $returnstr=$returnstr.substr($sourcestr,$i,1);
+         $i=$i+1;            //实际的Byte数仍计1个
+         $n++;            //但考虑整体美观，大写字母计成一个高位字符
+      }
+      else                //其他情况下，包括小写字母和半角标点符号，
+      {
+         $returnstr=$returnstr.substr($sourcestr,$i,1);
+         $i=$i+1;            //实际的Byte数计1个
+         $n=$n+0.5;        //小写字母和半角标点等与半个高位字符宽...
+      }
+   }
+         if ($str_length>$cutlength){
+          $returnstr = $returnstr . "...";//超过长度时在尾处加上省略号
+      }
+    return $returnstr; 
+
+}
+
+	$catid = get_cat_ID("置顶文章");
+
+    $args = array('numberposts'=>2, 'category'=>$catid);
+    $myposts = get_posts( $args );
+    foreach( $myposts as $post ){
+      $pid= $post->ID;
+      //echo $post->ID;
+      //echo "title=".$post->post_title;
+      $ptitle = $post->post_title;
+      $plink = get_permalink($pid);
+      $pcontent = chinasuber($post->post_content, 240);
+      echo "<h2><a href=\"$plink\">$ptitle</a></h2>";
+      echo "<p>$pcontent";
+      echo "<a href=\"$plink\">阅读全部</a></p><br/>";
+    }
+  
+?>
+</div>
+<br/>
 
 						   <?php
 
